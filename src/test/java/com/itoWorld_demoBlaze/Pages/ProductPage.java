144997c7ce45d38a688b0e2f0611983c6ab8404d
Sinan Yilmaz <cakimentoglu@gmail.com>
2024-02-12 01:18:03 +0000

    handling some alert for edge browser

--- src/test/java/com/itoWorld_demoBlaze/Pages/ProductPage.java
+++ src/test/java/com/itoWorld_demoBlaze/Pages/ProductPage.java
@@ -1,12 +1,15 @@
 package com.itoWorld_demoBlaze.Pages;
 
+import com.itoWorld_demoBlaze.Utilities.AppTestBase;
 import com.itoWorld_demoBlaze.Utilities.CommonMethod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class ProductPage extends CommonPage{
+public class ProductPage extends CommonPage {
 
     private static final Logger log = LogManager.getLogger(HomePage.class.getName());
 
@@ -16,10 +19,16 @@ public class ProductPage extends CommonPage{
     @FindBy(css = ".btn-success")
     private WebElement addToCartBtn;
 
-    public void addToCart(){
+    public void addToCart() {
         CommonMethod.clickWithJS(addToCartBtn);
-        CommonMethod.waitForPageToLoad();
-        log.info(itemName.getText() + " has been added to the Cart");
-        CommonMethod.waitForPageToLoad();
+        CommonMethod.waitFor(2);
+        try {
+            Alert alert = AppTestBase.getDriver().switchTo().alert();
+            String alertText = alert.getText();
+            log.info("Alert message: " + alertText);
+            alert.accept();
+        } catch (NoAlertPresentException e) {
+            log.info(itemName.getText() + " has been added to the Cart");
+        }
     }
 }
